ff18cf06eb45487ecb479f92f593030cc3256e67
Flávio <flavio@dizain.com.br>
2017-03-23 15:31:53 -0300

    ajuste slider

--- wp-content/themes/top2you-black/front-page.php
+++ wp-content/themes/top2you-black/front-page.php
@@ -28,8 +28,10 @@
 					      			</div>
 					      		</div>
 					      		<div class="text-mob">
-					      			<h2><?php echo $slide['speaker']; ?></h2>
-					      			<h3><?php echo $slide['cargo']; ?></h3>
+					      			<div class="container">
+					      				<h2><?php echo $slide['speaker']; ?></h2>
+					      				<h3><?php echo $slide['cargo']; ?></h3>
+					      			</div>
 					      		</div>
 					    	</div>
 				  			<?php
